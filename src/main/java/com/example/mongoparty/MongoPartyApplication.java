ce84d9426d0ffc2c286eb979fe37ce3a128fa0e2
Bohdan Koval <bohkov23@gmail.com>
2023-09-07 14:44:20 +0300

    init commit

---
+++ src/main/java/com/example/mongoparty/MongoPartyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongoparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoPartyApplication.class, args);
+    }
+
+}
